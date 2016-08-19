a6744371accd7a0cc95234c7dad6429c4b8754df
ilakkiadevi <kilakkiaspm@gmail.com>
2016-08-19 14:10:19 +0530

    Create c.java

---
+++ c.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class c
+{
+public static void main(String[] args)
+Scanner s=new Scanner(System.in);
+String str=s.next();
+StringBuffer str1=new StringBuffer(str);
+System.out.println(str1.reverse());
+
+}
+}
